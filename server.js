01576ce62608232a5f40b1f96366ae22c1003fc5
bharad-stack <51435868+bharad-stack@users.noreply.github.com>
2019-09-07 00:22:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -20,11 +20,38 @@ app.listen(3000, () => {
 	console.log("Server running on port 3000");
 });
 
+//request type
+app.get('/', function (request, response) {
+	var res = [{"request_name":"addroom",
+	"url":"http://localhost:3000/add_room",
+	"request_type":"POST",
+	"request_format":{"room_name":"default_room","room_points":0,"status":"booked","booked_user":"name"}},
+	{"request_name":"adduser",
+	"url": "http://localhost:3000/add_user",
+	"request_type":"POST",
+	"request_format":{"user_name":"name","points": 100}},
+	{"request_name":"bookroom",
+	"url":"http://localhost:3000/book_room",
+	"request_type":"POST",
+	"request_format":{"user_name":"name","room_name":"name"}},
+	{"request_name":"adduserpoints",
+	"url":"http://localhost:3000/add_user_points",
+	"request_type":"POST",
+	"request_format":{"user_name":"name","points": 100}},
+	{"request_name":"listrooms",
+	"request_type":"GET",
+	"url":"http://localhost:3000/list_rooms"},
+	{"request_name":"listusers",
+	"request_type":"GET",
+	"url":"http://localhost:3000/list_users"}]
+	response.json(res); // echo the result back
+});
+
 //add_user
 app.post('/add_user', function (request, response) {
 	
 	if (request.body.user_name == undefined || request.body.points == undefined) {
-		response.send("invalid request. use get request /request_types rest point to know the request body details.");
+		response.send("invalid request. use get request http://$ipaddress:3000/ rest point to know the request body details.");
 	}
 	else {
 		for (var i = 0; i < users.length; i++) {
@@ -36,7 +63,7 @@ app.post('/add_user', function (request, response) {
 			response.send("userid already exist.");
 		} else {
 			users[users.length] = request.body;
-			console.log(users);
+			//console.log(users);
 			response.send(users);
 		}
 	}
@@ -45,9 +72,9 @@ app.post('/add_user', function (request, response) {
 
 //add points
 app.post('/add_user_points', function (request, response) {
-	console.log(request.body.user_name + request.body.points);
+	//console.log(request.body.user_name + request.body.points);
 	if (request.body.user_name == undefined || request.body.points == undefined) {
-		response.send("invalid request. use get request /request_types rest point to know the request body details.");
+		response.send("invalid request. use get request http://$ipaddress:3000/ rest point to know the request body details.");
 	} else {
 		for (var i = 0; i < users.length; i++) {
 			if (users[i].user_name == request.body.user_name) {
@@ -56,14 +83,12 @@ app.post('/add_user_points', function (request, response) {
 		}
 		if (i < users.length) {
 			users[i].points += request.body.points;
-			//console.log(users);
 			response.send(users);
 		} else {
 			response.send("user_name doesn't exist.");
 		}
 	}
-	//console.log(request.body);      
-	//response.send(request.body);   
+	 
 });
 
 
@@ -75,7 +100,7 @@ app.get("/list_users", (req, res, next) => {
 //add rooms
 app.post('/add_room', function (request, response) {
 	if (request.body.room_name == undefined || request.body.room_points == undefined || request.body.status == undefined || request.body.booked_user == undefined) {
-		response.send("invalid request. use  get request /request_types rest point to know the request body details.");
+		response.send("invalid request. use  get request http://$ipaddress:3000/ rest point to know the request body details.");
 	} else {
 		for (var i = 0; i < rooms.length; i++) {
 			if (rooms[i].room_name == request.body.room_name) {
@@ -86,48 +111,44 @@ app.post('/add_room', function (request, response) {
 			response.send("Room already exist.");
 		} else {
 			rooms[rooms.length] = request.body;
-			//console.log(users);
 			response.send(rooms);
 		}
 	}
-	//console.log(request.body);     
-	//response.send(request.body);    
+ 
 });
 
 //book room
 
 app.post('/book_room', function (request, response) {
-	//console.log(request.body.user_name+request.body.room_name);
+	
 	if (request.body.user_name == undefined || request.body.room_name == undefined) {
-		response.send("invalid request. use get request /request_types rest point to know the request body details.");
+		response.send("invalid request. use get request http://$ipaddress:3000/ rest point to know the request body details.");
 	} else {
 		for (var i = 0; i < users.length; i++) {
-			console.log(users[i].user_name + request.body.user_name);
+			//console.log(users[i].user_name + request.body.user_name);
 			if (users[i].user_name == request.body.user_name) {
 				break;
 			}
 		}
 		for (var j = 0; j < rooms.length; j++) {
-			//console.log(rooms[j].room_name + request.body.room_name);
+			
 			if (rooms[j].room_name == request.body.room_name) {
 				break;
 			}
 		}
-		console.log(i, j);
+		//console.log(i, j);
 		if (j >rooms.length || i >users.length) {
 			response.send("username or roomname not found");
 		} else if (rooms[j].room_points > users[i].points || rooms[j].status == "booked") {
 			response.send("PENDING APPROVAL");
 		} else {
-			console.log(users[i].points);
 			users[i].points = users[i].points - rooms[j].room_points;
 			rooms[j].status = "booked";
 			rooms[j].booked_user = users[i].user_name;
 			response.send("BOOKED");
 		}
 	}
-	//console.log(request.body);   
-	//response.send(request.body);    
+	 
 });
 
 
@@ -136,8 +157,3 @@ app.get("/list_rooms", (req, res, next) => {
 	res.json(rooms);
 });
 
-//request type
-app.get('/request_types', function (request, response) {
-	var res = '##add_room request type(POST) => {"room_name":"default_room","room_points":0,"status":"booked","booked_user":"default"}  ##add_user request type(POST) =>{"user_name":"a","points": 100}  ##book_room requesttype(POST)=>{"user_name":"name","room_name":"name"} ##list_rooms,list_users(GET)';
-	response.json(res); // echo the result back
-});
